df2d795fde528054f1c71454c317237f9ffd24aa
shahreyar46 <shahreyar.shimul15@gmail.com>
2022-07-18 18:07:32 +0600

    fixed free shipping bar css issues

--- modules/free_shipping_bar/assets/js/frontend.js
+++ modules/free_shipping_bar/assets/js/frontend.js
@@ -18,12 +18,7 @@ var $wceazyFFsb = {
 
             //load fb handalers here ..
             $wceazyFFsb.init_wceazyfsb_content();
-            $wceazyFFsb.init_wceazyfspbar_content();
-            
-            //init close handaler
-            setTimeout( function(){
-                $wceazyFFsb.wceazy_handle_close_fsb();
-            },700 );            
+            $wceazyFFsb.init_wceazyfspbar_content();           
             
         });
 
@@ -163,6 +158,10 @@ var $wceazyFFsb = {
 
 };
 
+
+function wceazy_handle_close_fsb () {
+    $('#wceazy_init_free_shipping_bar').fadeOut('slow'); 
+ }
 
 //load js for frontend
 $wceazyFFsb.init_wceazyffsb();
